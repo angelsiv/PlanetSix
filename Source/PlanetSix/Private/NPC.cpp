c2a10ff2e17aa47ff7bd5114b4f3b7d03a51b45f
Youcef <mekki.d.youcef@outlook.fr>
2020-06-09 19:16:44 -0400

    Modification On NPCQUEST
    Visual work  to see if NPC has a quest Available Or No

--- Source/PlanetSix/Private/NPC.cpp
+++ Source/PlanetSix/Private/NPC.cpp
@@ -17,8 +17,11 @@ ANPC::ANPC()
 	RootComponent = boxcomponent;
 	
 	//Declare TextRender
-	textrender = CreateDefaultSubobject<UTextRenderComponent>(TEXT("TEXTRENDER"));
-	textrender->AttachToComponent(boxcomponent, FAttachmentTransformRules::KeepRelativeTransform);
+	textrenderInteraction = CreateDefaultSubobject<UTextRenderComponent>(TEXT("TEXTRENDERInteraction"));
+	textrenderInteraction->AttachToComponent(boxcomponent, FAttachmentTransformRules::KeepRelativeTransform);
+
+	textrenderQuest = CreateDefaultSubobject<UTextRenderComponent>(TEXT("TEXTRENDERQuest"));
+	textrenderQuest->AttachToComponent(boxcomponent, FAttachmentTransformRules::KeepRelativeTransform);
 
 	// Declaring Skeleton of Npc
 	skeleton = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletonMesh"));
@@ -30,18 +33,24 @@ ANPC::ANPC()
 void ANPC::BeginPlay()
 {
 	Super::BeginPlay();
-
+	
 	//Set the visibility of the text Press F to Interact to true 
-	textrender->SetVisibility(false);
-
+	textrenderInteraction->SetVisibility(false);
+	bOnInteraction = false;
+	
 	if (AnimIdle) 
 	{
 		//Play an animtion on begin play for the NPC 
 		skeleton->PlayAnimation(AnimIdle, true);
 	}
-	
 
-	bOnInteraction = false;
+	if (QuestID.IsNone()) 
+	{
+		print("No Quest Assigned for the NPC  ", 9);
+		textrenderQuest->SetVisibility(false);
+
+	}
+	
 	//Call The child actors attached to the NPC 
 	static const FString ContextString(TEXT("QuestDataTableCpp"));
 	QuestDataPointer = QuestDatatable->FindRow<FQuestData>(QuestID, ContextString, true);
@@ -55,11 +64,6 @@ void ANPC::BeginPlay()
 		NPCQuest.QuestTitleName = QuestDataPointer->QuestTitleName;
 
 		print("Validating " + NPCQuest.QuestTitleName.ToString(), 9);
-
-		//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Yellow, TEXT("DATA TABLE BEING READ "));
-		//Set the variables of the quests TEXT Title and Text Description 
-	/*	QuestDescriptionText = QuestDataPointer->QuestDescription;
-		QuestNameText = QuestDataPointer->QuestTitleName;*/
 
 	}
 
