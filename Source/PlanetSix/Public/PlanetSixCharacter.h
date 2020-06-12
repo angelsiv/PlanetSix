de0d0fbd0ba347b9e28931bc6afc66b7989e87ed
Youcef <mekki.d.youcef@outlook.fr>
2020-06-12 18:54:22 -0400

    Working Track of Quest Accepted
    so now when the player accepts a quest its possible to see what are the objectives of the quest accepted 
    For the Player Blueprint i only added a create widget to the begin Play

--- Source/PlanetSix/Public/PlanetSixCharacter.h
+++ Source/PlanetSix/Public/PlanetSixCharacter.h
@@ -63,6 +63,14 @@ public:
 		TSubclassOf<UUserWidget> QuestWidgetLog;
 
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CurrentQuest")
+		TSubclassOf<UUserWidget> CurrentQuestWidget;
+
+
+	UPROPERTY(BlueprintReadWrite)
+		UUserWidget* CurrentQuest;
+
+
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "QuestCompleted")
 		TSubclassOf<UUserWidget> QuestCompletedClass;
@@ -89,6 +97,8 @@ public:
 	//Reference to QuestBoardWidget
 	UPROPERTY(BlueprintReadWrite, Category = "QuestBoard")
 		UQuestBoardWidget* QuestBoardWidget;
+
+
 
 #pragma endregion
 
