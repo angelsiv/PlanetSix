ac12bdb5cdd2b47d4969cd18015d3075637cb127
Youcef <mekki.d.youcef@outlook.fr>
2020-07-20 11:50:16 -0400

    Added new widget C++ and work on UI

---
+++ Source/PlanetSix/Public/CurrentQuestTracker.h
@@ -0,0 +1,23 @@
+// This work is the sole property of 2Pow6 Games.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Runtime/UMG/Public/UMG.h"
+#include "Engine.h"
+#include "Components/EditableTextBox.h"
+#include "CurrentQuestTracker.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLANETSIX_API UCurrentQuestTracker : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+		UTextBlock* Objectives;
+	
+};
